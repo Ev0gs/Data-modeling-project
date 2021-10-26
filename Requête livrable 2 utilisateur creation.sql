04a9ed1fe83e1650d019a60b40e20391ed136ec8
Ev0gs <93186642+Ev0gs@users.noreply.github.com>
2021-10-26 12:18:53 +0200

    Delete Requête livrable 2 utilisateur creation.sql

--- Requête livrable 2 utilisateur creation.sql
+++
@@ -1,5 +0,0 @@
-DROP USER IF EXISTS 'user'@'localhost';
-CREATE USER 'user'@'localhost' IDENTIFIED BY '123';
-GRANT CREATE ON *.* TO 'user'@'localhost';
-GRANT ALL PRIVILEGES ON projet_final.* TO 'user'@'localhost';
-SHOW GRANTS FOR 'user'@'localhost';
